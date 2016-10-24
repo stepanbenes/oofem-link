d8f1adf3397202f174c588dc81149d1c9f301aaf
Štěpán Beneš <benes.stepan@gmail.com>
2016-10-24 19:19:06 +0200

    Add reference to unofficial CommandLineParser library

---
+++ src/OofemLink.Console/CommandLineOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace OofemLink.Console
+{
+    abstract class CommandLineOptions
+    {
+		[Option(Required = false, HelpText = "Prints all messages to standard output.")]
+		public bool Verbose { get; set; }
+	}
+
+	[Verb("import", HelpText = "Import model and mesh data to OOFEM database")]
+	class ImportOptions : CommandLineOptions
+	{
+		public enum ModelFormat
+		{
+			SEN = 0, // Scia ENgineer
+			Default = SEN,
+		}
+
+		[Option('f', "format", Required = false, HelpText = "Format of model to import, options: \"SEN\" for Scia-Engineer (default)")]
+		public ModelFormat Format { get; set; }
+
+		[Option('l', "location", Required = false, HelpText = "Location of input directory (current directory is used if none provided)")]
+		public string Location { get; set; }
+	}
+}
--- src/OofemLink.Console/Program.cs
+++ src/OofemLink.Console/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandLine;
+using static System.Console;
 
-namespace OofemLink
+namespace OofemLink.Console
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-        }
-    }
+			return Parser.Default.ParseArguments<ImportOptions>(args)
+					.MapResult(
+						(ImportOptions options) => runImportCommand(options),
+						errors => 1);
+		}
+
+		private static int runImportCommand(ImportOptions options)
+		{
+			if (options.Verbose)
+			{
+				var location = options.Location ?? Directory.GetCurrentDirectory();
+				WriteLine("Location: " + location);
+				WriteLine("Format: " + options.Format);
+			}
+			return 0;
+		}
+	}
 }
